a52da919e9a1268f6508b860def9327b39672ebb
Андрей <133521547+AndrejSH1981@users.noreply.github.com>
2024-03-02 13:01:35 +0300

    Update Kontr_8-Cpp-ITMO.cpp
    Исправленный код

--- Kontr_8-Cpp-ITMO/Kontr_8-Cpp-ITMO.cpp
+++ Kontr_8-Cpp-ITMO/Kontr_8-Cpp-ITMO.cpp
@@ -1,42 +1,18 @@
-﻿// Kontr_8-Cpp-ITMO.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Kontr_8-Cpp-ITMO.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <cmath>
 # include <windows.h>
 #include <iomanip>
 #include <iostream>
-#include <format>
-#include <stdexcept>
-#include <string>
-#include <sstream>
-
-
 
 using namespace std;
-
-//// Количество секунд в минуте
-//int SecondsInMinute = 60;
-//
-//// Количество минут в часе
-//int MinutesInHour = 60;
-//
-//// Количество секунд в часе
-//int SecondsInHour = SecondsInMinute * MinutesInHour;
-//
-//// Количество часов в дне
-//int HoursInDay = 24;
-//
-//// Количество секунд в дне
-//int SecondsInDay = SecondsInHour * HoursInDay;
-
-
 
 // Время в формате часы, минуты, секунды
 class Time {
 public:
 
     int hours;
- 
+
     int minutes;
 
     int seconds;
@@ -48,38 +24,40 @@ public:
     }
 
     // Для преобразование или вычитания времени к или от другому времени
-    int const To_Time_Other() {
+    int To_Time_Other() {
         int a2b2c2 = 0;
         a2b2c2 = CheckingTime(hours, minutes, seconds);
         int hours1 = a2b2c2 / 10000;
         int minutes1 = (a2b2c2 % 10000) / 100;
         int seconds1 = a2b2c2 % 100;
+      
         return hours1 * 10000 + minutes1 * 100 + seconds1 * 1;
+
     }
 
+    Time Result(int resultTime) {
+
+        int hours11 = resultTime / 10000;
+        int minutes11 = (resultTime % 10000) / 100;
+        int seconds11 = resultTime % 100;
+        int a2b2c2 = 0;
+        a2b2c2 = CheckingTime(hours11, minutes11, seconds11);
+        int hours22 = a2b2c2 / 10000;
+        int minutes22 = (a2b2c2 % 10000) / 100;
+        int seconds22 = a2b2c2 % 100;
+        return Time(hours22, minutes22, seconds22);
+    }
 
     // Прибавление времени
-    int Add( Time& other) {
-        int(other.To_Time_Other() + To_Time_Other());
-        int aa = int(other.To_Time_Other() + To_Time_Other());
-        int hours11 = aa / 10000;
-        int minutes11 = (aa % 10000) / 100;
-        int seconds11 = aa % 100;     
-       int result = 0;
-       result = CheckingTime(hours11, minutes11, seconds11);
-        return result;        
+    Time Add(Time& other) {
+        Time S = Result(other.To_Time_Other() + To_Time_Other());   
+        return S;
     }
 
     // Вычитание времени
-    int Sub(Time& other) {
-        int(To_Time_Other() - other.To_Time_Other());
-        int aa = int(To_Time_Other() - other.To_Time_Other());
-        int hours11 = aa / 10000;
-        int minutes11 = (aa % 10000) / 100;
-        int seconds11 = aa % 100;
-        int result = 0;
-        result = CheckingTime(hours11, minutes11, seconds11);
-        return result;
+    Time Sub(Time& other) {
+        Time S = Result(To_Time_Other() - other.To_Time_Other());
+        return S;
     }
 
 
@@ -97,10 +75,16 @@ public:
         bool marker6 = false;
         bool marker7 = false;
 
-        if (a1 == 0 && b1 == 0 && c1 == 0) {
-            a1 = a;
-            b1 = b;
-            c1 = c;
+        if (a1 == 0) {
+            a1 = 24;
+           marker1 = true;
+        }
+        if (b1 == 0) {
+            b1 = 60;
+            marker1 = true;
+        }
+        if (c1 == 0) {
+            c1 = 60;
             marker1 = true;
         }
         if (a1 < 0) {
@@ -133,7 +117,7 @@ public:
         }
         else {
             marker4 = false;
-        }     
+        }
         if (b1 >= 60) {
             a1 = a1 + 1;
             b1 = b1 - 60;
@@ -141,7 +125,7 @@ public:
         }
         else {
             marker3 = false;
-        }     
+        }
         if (a1 >= 24) {
             a1 = a1 - 24;
             marker2 = true;
@@ -156,7 +140,7 @@ public:
             printf("%02d", b1);
             cout << ":";
             printf("%02d", c1);
-            cout << " " << endl;
+            cout << " ";
             marker1 == false;
             int a1b1c1 = a1 * 10000 + b1 * 100 + c1 * 1;
             return a1b1c1;
@@ -166,6 +150,30 @@ public:
             return a1b1c1;
         }
     };
+
+// [out] time-Время, в которое будет считана информация
+    friend istream& operator>>(istream& in, Time& time) {
+        in >> time.hours;
+        in.get();
+        in >> time.minutes;
+        in.get();
+        in >> time.seconds;
+        int a3b3c3 = 0;
+        a3b3c3 = time.hours * 10000 + time.minutes * 100 + time.seconds * 1;
+        time.Result(a3b3c3);
+
+        return in;
+    }
+
+// time-Время, которое нужно вывести в поток
+    friend ostream& operator<<(ostream& out, const Time& time) {
+        out << setw(2) << setfill('0') << time.hours;
+        out << ':';
+        out << setw(2) << setfill('0') << time.minutes;
+        out << ':';
+        out << setw(2) << setfill('0') << time.seconds;
+        return out;
+    }
 
 private:
     Time(int hours, int minutes, int seconds, bool validate) : hours(hours), minutes(minutes), seconds(seconds) {
@@ -177,57 +185,27 @@ int main() {
 
     setlocale(LC_CTYPE, "rus");
 
-    wcout << L"Введите первое время \"HH:MM:SS\" поочереди: " << endl;
-    int t1_hours = 0;
-    int t1_minutes = 0;
-    int t1_seconds = 0;
+    wcout << L"Введите первое время \"HH:MM:SS\" через пробел или поочереди: " << endl;
+    Time t1_time;
+    wcout << L" - \"HH:MM:SS\:";
+    cin >> t1_time;
+    wcout << L"Введите второе время \"HH:MM:SS\" через пробел или поочереди: " << endl;
 
-    wcout << L" - часы \"HH\":";
-    cin >> t1_hours;
-    wcout << L" - минуты \"MM\":";
-    cin >> t1_minutes;
-    wcout << L" - секунды \"SS\":";
-    cin >> t1_seconds;
-    Time t1 = Time(t1_hours, t1_minutes, t1_seconds);
-   
-    cout << "Введите второе время \"HH:MM:SS\" поочереди: : " << endl;
+    Time t2_time;
+    wcout << L" - \"HH:MM:SS\:";
+    cin >> t2_time;
 
-    int t2_hours = 0;
-    int t2_minutes = 0;
-    int t2_seconds = 0;
-    wcout << L" - часы \"HH\":";
-    cin >> t2_hours;
-    wcout << L" - минуты \"MM\":";
-    cin >> t2_minutes;
-    wcout << L" - секунды \"SS\":";
-    cin >> t2_seconds;
-    Time t2 = Time(t2_hours, t2_minutes, t2_seconds);
+    Time time_result = t1_time.Add(t2_time);
 
-    int int_result = int(t1.Add(t2));
-    int hours_result = int_result / 10000;
-    int minutes_result = (int_result % 10000) / 100;
-    int seconds_result = int_result % 100;
+    wcout << L"\n- первое время + второе время = ";
+    cout << time_result << endl;
+    cout << "--------------" << endl;
 
-    wcout << L"- первое время + второе время = ";
-    printf("%02d", hours_result);
-    cout << ":";
-    printf("%02d", minutes_result);
-    cout << ":";
-    printf("%02d", seconds_result);
-    cout << " " << endl;
+    time_result = t1_time.Sub(t2_time);
+    wcout << L"\n- первое время - второе время = ";
+    cout << time_result << endl;
+    cout << "--------------" << endl;
 
-    int_result = int(t1.Sub(t2));
-    hours_result = int_result / 10000;
-    minutes_result = (int_result % 10000) / 100;
-    seconds_result = int_result % 100;
-
-    wcout << L"- первое время - второе время = ";
-    printf("%02d", hours_result);
-    cout << ":";
-    printf("%02d", minutes_result);
-    cout << ":";
-    printf("%02d", seconds_result);
-    cout << " " << endl;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
@@ -242,13 +220,5 @@ int main() {
 //   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
 
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
 //   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
 //   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
